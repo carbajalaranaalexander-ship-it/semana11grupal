58eba44e15640e1479321b08ba8e889efa074287
Rolando102 <145702522+Rolando102@users.noreply.github.com>
2026-06-18 10:35:55 -0500

    Delete catalogo_productos_tecnologicos directory

--- catalogo_productos_tecnologicos/catalogo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Producto
-
-
-@admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = ("nombre", "categoria", "precio", "stock", "creado_en")
-    list_filter = ("categoria",)
-    search_fields = ("nombre",)
--- catalogo_productos_tecnologicos/catalogo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# Cada ruta usa 'name=' para poder referenciarla en las plantillas con
-# {% url 'home' %} y {% url 'catalogo' %}, desacoplando las URLs del HTML.
-urlpatterns = [
-    path('', views.home, name='home'),                          # FBV bienvenida
-    path('catalogo/', views.ProductoListView.as_view(), name='catalogo'),  # CBV ListView
-]
